21c35a918d3be50ddedca5953674e785f46ef1f8
Whitney Nicole Stotler <user@Whitney-Stotlers-Macbook-Pro.local>
2018-01-17 22:53:12 -0600

    new attempt,still broke fml

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+var path = require('path');
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+  	rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      {
+        test: require.resolve('path/to/answer.js'),
+        use: [
+          {
+            loader: 'val-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}  
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+	  },
+	  {
+        test: /\.exec\.js$/,
+        use: [ 'script-loader' ]
+      },
+      {
+        test: /\.html$/,
+        use: [ "html-loader" ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.json5$/,
+        loader: 'json5-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'env', 'react']
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8080
+  }
+};
